c2da5111bc657f7c1571c0e5b49b7a26499aeda1
BlueZeeKing24 <52867955+BlueZeeKing@users.noreply.github.com>
2022-02-17 15:21:20 -0500

    fixed handeling of no message

--- pages/api/aps.js
+++ pages/api/aps.js
@@ -4,9 +4,14 @@ export default function handler(req, res) {
   request('https://www.apsva.us/wp-json/wp/v2/mat_alert', function (error, response, body) {
     let data = JSON.parse(body)
     let outData = {}
-    outData['title'] = data[0]['title']['rendered']
-    outData['link'] = data[0]['link']
-    console.log(outData)
+    if (data.length == 0) {
+      outData['title'] = ""
+      outData['link'] = ""
+    } else {
+      outData['title'] = data[0]['title']['rendered']
+      outData['link'] = data[0]['link']
+    }
+    outData['available'] = data.length > 0
     res.status(200).send(JSON.stringify(outData));
   })
 }
