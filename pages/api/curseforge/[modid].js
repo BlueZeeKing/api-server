c48399b272a110f15720947fd45c2b932d970c79
BlueZeeKing <52867955+BlueZeeKing@users.noreply.github.com>
2022-04-16 01:00:58 -0400

    Add gameversion and mod version

--- pages/api/curseforge/[modid].js
+++ pages/api/curseforge/[modid].js
@@ -8,9 +8,14 @@ export default async function handler(req, res) {
     }
   );
 
+  const latestFile = response.data.data.latestFiles.filter(item => item.id == response.data.data.mainFileId)[0];
+  const latestFileIndex = response.data.data.latestFilesIndexes.filter(item => item.fileId == response.data.data.mainFileId)[0];
+
   res.status(response.status).send(
     JSON.stringify({
       downloads: response.data.data.downloadCount,
+      gameVersion: latestFileIndex.gameVersion,
+      version: latestFile.displayName,
     })
   );
 }
