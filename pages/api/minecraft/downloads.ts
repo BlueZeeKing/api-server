31207175dcd6a5dfc5ff30127d6faeaa69306bda
BlueZeeKing <github@blueish.dev>
2022-06-25 13:13:31 -0400

    Update to include modrinth is download stats move api url

---
+++ pages/api/minecraft/downloads.ts
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+export default async function handler(req, res) {
+  const curseResponse = await axios.get(
+    `https://api.curseforge.com/v1/mods/${req.query.curseid}`,
+    {
+      headers: { "x-api-key": process.env.API_KEY },
+    }
+  );
+
+  const modrinthResponse = await axios.get(
+    `https://api.modrinth.com/v2/project/${req.query.modrinthid}`,
+    {
+      headers: { "x-api-key": process.env.API_KEY },
+    }
+  );
+
+  console.log(modrinthResponse.data.downloads);
+
+  res.status(200).send(
+    JSON.stringify({
+      downloads:
+        curseResponse.data.data.downloadCount + modrinthResponse.data.downloads,
+    })
+  );
+}
